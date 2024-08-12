58b8bb528f56632ba2db13564b76b4ed9da19aed
Anup Rai <dev8@esignature.com.np>
2024-08-12 09:58:06 +0545

    feat: add solutions for 'Return value' problems

--- src/00_about_return_value.sh
+++ src/00_about_return_value.sh
@@ -1,11 +1,15 @@
 lesson_title "Return value"
 
 test_return_value() {
+  # &> redirects both the stdout and stderr to /dev/null
+  # since we don't have a folder named 'ZOMGNODIRLIKETHIS', the 'cd' command will fail, and result in an exit code of 1
   cd /ZOMGNODIRLIKETHIS &> /dev/null
 
-  assertEqual $? __
+  # $? contains the exit status code of the last command executed
+  assertEqual $? 1
 
+  # this won't fail because we are executing the 'ls' command on the home folder
   ls ~/ > /dev/null
 
-  assertEqual $? __
+  assertEqual $? 0
 }
